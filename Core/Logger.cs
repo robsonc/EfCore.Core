2810d1d796415462d29ba8d1e806d85205265d28
Robson Coutinho <robson.coutinho@am4.com.br>
2021-04-07 19:22:53 -0300

    update

---
+++ Core/Logger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Core
+{
+    public class Logger
+    {
+        public void LogInformation(string info)
+        {
+            if (info == null) throw new ArgumentNullException(nameof(info));
+            Console.WriteLine(info);
+        }
+    }
+}
